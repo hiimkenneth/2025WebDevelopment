88b40b136c2d38f895d3df773a3b96360fb82c0c
hiimkenneth <kennethy25@nycstudents.net>
2025-12-22 14:16:53 +0000

    practical 3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,18 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+
+function bananas(){
+    document.getElementById("highlight").innerHTML="Here's a whole big pile of bananas.";
+}
+function apples(){
+    document.getElementById("highlight").innerHTML="Here's a whole big pile of apples.";
+}
+function kiwis(){
+    document.getElementById("highlight").innerHTML="Here's a whole big pile of kiwis.";
+}
+function pineapples(){
+    document.getElementById("highlight").innerHTML="Here's a whole big pile of pineapples.";
+}
+function strawberries(){
+    document.getElementById("highlight").innerHTML="Here's a whole big pile of strawberries.";
+}
