7302a8263dd38cbadd082e8f0317d8b38960275b
hiimkenneth <kennethy25@nycstudents.net>
2026-01-08 14:01:41 +0000

    1/8

--- WDLesson3_4Practical/script.js
+++ WDLesson3_4Practical/script.js
@@ -17,24 +17,24 @@ function createStory(){
   Choose your variable names wisely.  This is a lot of practice of creating variables and using document.getElementById(..)
   */
 function createStory(){
-  let boyname = document.getElementById("boyname")
-  let vegetable1 = document.getElementById("vegetable1")
-  let girlname = document.getElementById("girlname")
-  let vegetable2 = document.getElementById("vegetable2")
+  let boyname = document.getElementById("boyname");
+  let vegetable1 = document.getElementById("vegetable1");
+  let girlname = document.getElementById("girlname");
+  let vegetable2 = document.getElementById("vegetable2");
 
 }
   /* Task 4: Set the innerHTML for each of the spans on lines 3 to 9 with the appropriate values from the variables you create in Task 3. */
-boyspan1.innerHTML = boyname
-boyspan2.innerHTML = boyname
-boyspan3.innerHTML = boyname
-boyspan4.innerHTML = boyname
+boy1span1.innerHTML = boyname.value;
+boy1span2.innerHTML = boyname.value;
+boy1span3.innerHTML = boyname.value;
+boy1span4.innerHTML = boyname.value;
 
-veg1.innerHTML = vegetable1
-veg2.innerHTML = vegetable2
+veg1.innerHTML = vegetable1.value;
+veg2.innerHTML = vegetable2.value;
 
-girlspan.innerHTML = girlname
+girlspan.innerHTML = girlname.value;
   /* Task 5: Set the display property of the element with an id of "story" to 'block' */
 
-story = document.getElementById("story").style.display = block
+document.getElementById("story").style.display = "block";
 
 }
