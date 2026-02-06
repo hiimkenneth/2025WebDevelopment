d07f8a5994b7a85730049d01cc17aed68e128b8c
hiimkenneth <kennethy25@nycstudents.net>
2026-02-06 13:58:06 +0000

    2/6

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -13,7 +13,21 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
+function area(){
+  let length = document.getElementById("length").value;
+  let width = document.getElementById("width").value
+  let output = document.getElementById("output");
 
+  let area = length * width
+
+  if (length <= 0 || width <= 0){
+    output.innerHTML = "Inappropriate measurement";
+  }else{
+    output.innerHTML = `The area of the rectangle is ${area}`;
+  }
+
+
+}
 
 
 /* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
@@ -21,5 +35,31 @@ function name(){
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
+  let animal = document.getElementById("animal").value
+  let emotion = document.getElementById("emotion").value
+  let filename = ""
 
+  let output = document.getElementById("output")
+
+if (animal == "Bear" && emotion == "Funny"){
+  filename = "funnyBear.jpg"
+}
+if (animal == "Cat" && emotion == "Funny"){
+  filename = "funnyCat.jpg"
+}
+if (animal == "Dog" && emotion == "Funny"){
+  filename = "funnyDog.jpg"
+}
+
+if (animal == "Bear" && emotion == "Sad"){
+  filename = "sadBear.jpg"
+}
+if (animal == "Cat" && emotion == "Sad"){
+  filename = "sadCat.jpg"
+}
+if (animal == "Dog" && emotion == "Sad"){
+  filename = "sadDog.jpg"
+}
+
+output.innerHTML = `<img src="${filename}">`
 }
